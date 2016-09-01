807f0ba5e8e7a6229d080ebaf20a299b5bc4e09d
mkodekar <rkodekar@zoho.com>
2016-09-02 00:36:52 +0530

    get size of varaible : sizeof examples

---
+++ sizeofvariables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+   
+  printf("max size of int is %d bytes\n\n", sizeof(int));
+  printf("max size of char is %d bytes\n\n", sizeof(char));
+  printf("max size of longint is %d bytes\n\n", sizeof(long));
+  printf("max size of float is %d bytes\n\n", sizeof(float));
+  printf("max size of double is %d bytes\n\n", sizeof(double));
+
+}
